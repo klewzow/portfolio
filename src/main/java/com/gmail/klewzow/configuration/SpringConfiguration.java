82e05d25b8546c6a0b2bf5f9bc3d92e749836346
Dmitry <klewzow@gmail.com>
2022-07-18 15:58:17 +0300

    ...git

--- src/main/java/com/gmail/klewzow/configuration/SpringConfiguration.java
+++ src/main/java/com/gmail/klewzow/configuration/SpringConfiguration.java
@@ -11,15 +11,57 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
-
+@Configuration
+@ComponentScan("com.gmail.klewzow")
+@EnableWebMvc
 public class SpringConfiguration implements WebMvcConfigurer {
 
-    private ApplicationContext applicationContext;
-    WebMvcConfigurationSupport webMvcConfigurationSupport;
-
+    private final ApplicationContext applicationContext;
+    @Autowired
     public SpringConfiguration(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
     }
 
+    /* ******************************************************************* */
+    /* GENERAL CONFIGURATION ARTIFACTS */
+    /* Static Resources, i18n Messages, Formatters (Conversion Service) */
+    /* ******************************************************************* */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**").addResourceLocations("classpath:/WEB-INF/image/");
+        registry.addResourceHandler("/style/**").addResourceLocations("classpath:/WEB-INF/style/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/WEB-INF/fonts/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
 
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("classpath:/WEB-INF/page/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.setTemplateResolver(templateResolver());
+        springTemplateEngine.setEnableSpringELCompiler(true);
+        return springTemplateEngine;
+    }
+
+    /*
+     * CONFIGURATION Thymeleaf ViewResolverRegistry
+     *
+     */
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(resolver);
+    }
 }
