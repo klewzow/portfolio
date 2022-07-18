26987660622176f0966e1b7816f0a24ac02c89b2
Dmitry <klewzow@gmail.com>
2022-07-18 15:53:17 +0300

    ...git

--- src/main/java/com/gmail/klewzow/configuration/SpringConfiguration.java
+++ src/main/java/com/gmail/klewzow/configuration/SpringConfiguration.java
@@ -5,10 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
@@ -20,52 +17,11 @@ import org.thymeleaf.templatemode.TemplateMode;
 public class SpringConfiguration implements WebMvcConfigurer {
 
     private ApplicationContext applicationContext;
-
+    WebMvcConfigurationSupport webMvcConfigurationSupport;
     @Autowired
     public SpringConfiguration(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
     }
 
-    /* ******************************************************************* */
-    /* GENERAL CONFIGURATION ARTIFACTS */
-    /* Static Resources, i18n Messages, Formatters (Conversion Service) */
-    /* ******************************************************************* */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/image/**").addResourceLocations("classpath:/WEB-INF/image/");
-        registry.addResourceHandler("/style/**").addResourceLocations("classpath:/WEB-INF/style/");
-        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/WEB-INF/fonts/");
-        WebMvcConfigurer.super.addResourceHandlers(registry);
-    }
 
-    @Bean
-    public SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("classpath:/WEB-INF/page/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setCharacterEncoding("UTF-8");
-        templateResolver.setCacheable(false);
-        return templateResolver;
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
-        springTemplateEngine.setTemplateResolver(templateResolver());
-        springTemplateEngine.setEnableSpringELCompiler(true);
-        return springTemplateEngine;
-    }
-
-    /*
-     * CONFIGURATION Thymeleaf ViewResolverRegistry
-     *
-     */
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-        resolver.setTemplateEngine(templateEngine());
-        registry.viewResolver(resolver);
-    }
 }
