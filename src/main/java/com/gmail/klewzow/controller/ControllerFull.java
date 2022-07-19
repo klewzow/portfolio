d8095e45f41dcc6aae147768f6afd5fddfe5baa9
Dmitry <klewzow@gmail.com>
2022-07-19 14:21:11 +0300

    ...init

--- src/main/java/com/gmail/klewzow/controller/ControllerFull.java
+++ src/main/java/com/gmail/klewzow/controller/ControllerFull.java
@@ -1,10 +1,12 @@
 package com.gmail.klewzow.controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@EnableAutoConfiguration
 public class ControllerFull {
 
 
